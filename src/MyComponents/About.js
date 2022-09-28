1d9360734c7612a726ca10f313f1b3d6e8094ed6
Bicku Kumar <bicku.kumar@tothenew.com>
2022-09-28 22:27:43 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        Lorem ipsum dolor sit amet. Ut voluptatem labore est laboriosam ullam
+        est odio quidem At laboriosam repellendus ut aspernatur atque. Aut ipsa
+        voluptatibus vel odit omnis et voluptatem commodi et libero omnis sed
+        minima sapiente. Est omnis corporis et consequatur quaerat ut recusandae
+        doloribus eos molestias maiores quo voluptates quis non nobis tenetur ut
+        obcaecati omnis. Aut ullam esse non corrupti laudantium et aliquid
+        provident ut quis fuga in architecto voluptas sed iusto exercitationem
+        est internos voluptatibus.
+      </p>
+    </div>
+  )
+}
